86d08015587ae1cb51472f687fe121486f49e0ff
Semih <semih.guner.2002@gmail.com>
2023-05-23 22:26:55 +0300

    Sizleri tanıyalım ekranında geliştirmeler yapıldı.

--- app/src/main/java/com/proje/kaloritakipuygulamasi/IlkKayitActivity.java
+++ app/src/main/java/com/proje/kaloritakipuygulamasi/IlkKayitActivity.java
@@ -2,13 +2,34 @@ package com.proje.kaloritakipuygulamasi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
+import java.util.Scanner;
 
 public class IlkKayitActivity extends AppCompatActivity {
 
+    AutoCompleteTextView autoCompleteTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ilk_kayit);
-    }
-}
+        autoCompleteTextView = findViewById(R.id.AutoCompleteCinsiyet);
+
+        String[] Cinsiyetler = new String[]{getString(R.string.erkek), getString(R.string.kadin)};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.dropdown_item, Cinsiyetler);
+        autoCompleteTextView.setAdapter(adapter);
+
+        //autoCompleteTextView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        //    @Override
+        //    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        //    }
+        //});
+
+    }}
