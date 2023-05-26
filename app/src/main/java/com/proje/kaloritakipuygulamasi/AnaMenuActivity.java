d531b54cc3b40180df07b56e4cb9db505ecdeefa
Semih <semih.guner.2002@gmail.com>
2023-05-26 13:01:50 +0300

    Menüler üzerine çalışmalar başladı.

--- app/src/main/java/com/proje/kaloritakipuygulamasi/AnaMenuActivity.java
+++ app/src/main/java/com/proje/kaloritakipuygulamasi/AnaMenuActivity.java
@@ -1,14 +1,56 @@
 package com.proje.kaloritakipuygulamasi;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
 
-public class AnaMenuActivity extends AppCompatActivity {
+import com.proje.kaloritakipuygulamasi.databinding.ActivityAnaMenuBinding;
+
+public class AnaMenuActivity extends AppCompatActivity{
+
+    ActivityAnaMenuBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ana_menu);
+        binding = ActivityAnaMenuBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        ReplaceFragment(new AnaMenuFragment());
+        binding.bottomNavigation.setBackground(null);
+
+        binding.bottomNavigation.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId())
+            {
+                case R.id.page1:
+                    ReplaceFragment(new AnaMenuFragment());
+                    break;
+                case R.id.page2:
+                    ReplaceFragment(new TakvimFragment());
+                    break;
+                case R.id.page3:
+                    ReplaceFragment(new UserFragment());
+                    break;
+                case R.id.page4:
+                    ReplaceFragment(new AyarlarFragment());
+                    break;
+            }
+
+            return true;
+        });
     }
+
+    private void ReplaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.Navbar_Layout, fragment);
+        fragmentTransaction.commit();
+    }
+
 }
---
+++ app/src/main/java/com/proje/kaloritakipuygulamasi/AnaMenuFragment.java
@@ -0,0 +1,20 @@
+package com.proje.kaloritakipuygulamasi;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class AnaMenuFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_ana_menu, container, false);
+    }
+}
