dd4461e5a3eb434e5cd4506a2850b717cb6fb869
Semih <semih.guner.2002@gmail.com>
2023-05-26 14:34:44 +0300

    Menüler üzerine çalışmalar devam ediyor.

--- app/src/main/java/com/proje/kaloritakipuygulamasi/AnaMenuFragment.java
+++ app/src/main/java/com/proje/kaloritakipuygulamasi/AnaMenuFragment.java
@@ -1,20 +1,29 @@
 package com.proje.kaloritakipuygulamasi;
 
+import android.graphics.drawable.RotateDrawable;
+import android.os.Build;
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 
 public class AnaMenuFragment extends Fragment {
 
+    View DisView;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        // View'ı fonksiyondan çıkartır.
+        View IcView = inflater.inflate(R.layout.fragment_ana_menu, container, false);
+        DisView = IcView;
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_ana_menu, container, false);
     }
+
 }
