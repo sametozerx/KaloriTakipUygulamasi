4fe964e332a5ea283a0f8b10bfffe683cabfaf2b
Semih <semih.guner.2002@gmail.com>
2023-05-27 12:38:43 +0300

    Takvim ekranı bitti.

--- app/src/main/java/com/proje/kaloritakipuygulamasi/TakvimFragment.java
+++ app/src/main/java/com/proje/kaloritakipuygulamasi/TakvimFragment.java
@@ -2,19 +2,38 @@ package com.proje.kaloritakipuygulamasi;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 
 public class TakvimFragment extends Fragment {
 
+    CalendarView takvim;
+    TextView yazi;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_takvim, container, false);
+        View view = inflater.inflate(R.layout.fragment_takvim, container, false);
+        takvim = (CalendarView) view.findViewById(R.id.takvim);
+        yazi = (TextView) view.findViewById(R.id.txtTarihlik);
+        takvim.setDate(System.currentTimeMillis(),false,true);
+
+        takvim.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                String tarih = dayOfMonth + "/" + (month+1) + "/" + year;
+                yazi.setText(tarih);
+            }
+        });
+        return view;
     }
 }
