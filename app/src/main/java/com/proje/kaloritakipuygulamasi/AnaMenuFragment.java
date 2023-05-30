f88bf871513e761cf919acde3fc991115b6ee60c
samet <samet.frost@gmail.com>
2023-05-30 11:15:04 +0300

    splash screnn ve userfragment yapıldı

--- app/src/main/java/com/proje/kaloritakipuygulamasi/AnaMenuFragment.java
+++ app/src/main/java/com/proje/kaloritakipuygulamasi/AnaMenuFragment.java
@@ -10,17 +10,27 @@ import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
+import com.proje.kaloritakipuygulamasi.database.KaloriTakipDatabase;
+import com.proje.kaloritakipuygulamasi.database.entities.Kullanici;
+
 
 public class AnaMenuFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        // View'ı fonksiyondan çıkartır.
+        KaloriTakipDatabase kaloridb = KaloriTakipDatabase.getKaloriTakipDatabase(requireContext());
+        Kullanici kullanici = kaloridb.kullaniciDao().loadFirstKullanici();
+
         View view = inflater.inflate(R.layout.fragment_ana_menu, container, false);
         TextView tv = (TextView) view.findViewById(R.id.txtYuzdeKalori);
         ProgressBar pb = (ProgressBar) view.findViewById(R.id.progressBar);
         String progress = Integer.toString(pb.getProgress());
         tv.setText("%" + progress);
+
+
+        TextView tv2 = (TextView) view.findViewById(R.id.txtSelamlama);
+        tv2.setText("İyi Günler, " + kullanici.getKullaniciAdi());
+
         return view;
     }
 
